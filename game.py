8958011e529297e2128f0535785995f883aeef63
Jonathan Taratuta-Titus <taratutatitus@college.harvard.edu>
2014-11-25 00:26:30 -0500

    created general valid move generator for person and computer

--- game.py
+++ game.py
@@ -11,12 +11,6 @@ class Move:
 	SOUTHWEST = 'Southwest'
 	SOUTHEAST = 'Southeast'
 
-	def validMove(input_move):
-		inputs = input_move.split(' ')
-		if len(inputs) is not 2:
-			return False
-		
-
 class Board:
 	def __init__(self):
 		self.state = [[0,-1,0,-1,0,-1,0,-1],
@@ -35,14 +29,76 @@ class Board:
 	def __str__(self):
 		""
 		representation = {-2: 'X ', -1: 'x ', 0:'_ ', 1:'o ', 2:'O '}
-		board = 'A B C D E F G H \n'
-		counter = 1
+		board = '0 1 2 3 4 5 6 7 \n'
+		counter = 0
 		for row in self.state:
 			for col in row:
 				board += representation[col]
 			board+= ' ' + str(counter) + ' \n'
 			counter += 1
 		return board 
+
+	def generateValidMoves(self, who):
+		valid_moves = []
+		valid_jumps = []
+		if who is 'computer':
+			for (x,y) in self.computer:
+				# SE
+				if self.state[x+1][y+1] == 0: # if empty square
+					valid_moves.append((x,y),'SE', 'M')
+				else if self.state[x+1][y+1] > 0 and self.state[x+2][y+2] == 0: # if can jump over
+					valid_jumps.append((x,y),'SE', 'J')
+				# SW
+				if self.state[x+1][y-1] == 0: # if empty square
+					valid_moves.append((x,y),'SW', 'M')
+				else if self.state[x+1][y-1] > 0 and self.state[x+2][y-2] == 0: # if can jump over
+					valid_jumps.append((x,y),'SW', 'J')
+				# if current square contains a king, consider north moves too
+				if self.state[x][y] == -2:
+					# NE
+					if self.state[x-1][y+1] == 0: # if empty square
+						valid_moves.append((x,y),'NE', 'M')
+					else if self.state[x-1][y+1] > 0 and self.state[x-2][y+2] == 0: # if can jump over
+						valid_jumps.append((x,y),'NE', 'J')
+					# NW
+					if self.state[x-1][y-1] == 0: # if empty square
+						valid_moves.append((x,y),'NW', 'M')
+					else if self.state[x-1][y-1] > 0 and self.state[x-2][y-2] == 0: # if can jump over
+						valid_jumps.append((x,y),'NW', 'J')
+			# if a jump can be made, the computer has to jump
+			if len(valid_jumps) > 0:
+				return valid_jumps
+			else:
+				return valid_moves
+		else: # if human
+			for (x,y) in self.human:
+				# if current square contains king, consider south moves too
+				if self.state[x][y] == 2:
+					# SE
+					if self.state[x+1][y+1] == 0: # if empty square
+						valid_moves.append((x,y),'SE', 'M')
+					else if self.state[x+1][y+1] < 0 and self.state[x+2][y+2] == 0: # if can jump over
+						valid_jumps.append((x,y),'SE', 'J')
+					# SW
+					if self.state[x+1][y-1] == 0: # if empty square
+						valid_moves.append((x,y),'SW', 'M')
+					else if self.state[x+1][y-1] < 0 and self.state[x+2][y-2] == 0: # if can jump over
+						valid_jumps.append((x,y),'SW', 'J')
+				# NE
+				if self.state[x-1][y+1] == 0: # if empty square
+					valid_moves.append((x,y),'NE', 'M')
+				else if self.state[x-1][y+1] < 0 and self.state[x-2][y+2] == 0: # if can jump over
+					valid_jumps.append((x,y),'NE', 'J')
+				# NW
+				if self.state[x-1][y-1] == 0: # if empty square
+					valid_moves.append((x,y),'NW', 'M')
+				else if self.state[x-1][y-1] < 0 and self.state[x-2][y-2] == 0: # if can jump over
+					valid_jumps.append((x,y),'NW', 'J')
+			# if a jump can be made, the person has to jump
+			if len(valid_jumps) > 0:
+				return valid_jumps
+			else:
+				return valid_moves
 
 	def generateSuccessorBoard(self, pos, direction):
 		x, y = pos
@@ -70,6 +126,9 @@ class Game:
 			
 			x, y = raw_input("Choose piece (format: x y): ").split()
 			direction = raw_input("Choose direction (format: NE or NW or SE or SW): ")
+			moves = generateValidMoves('human')
+			if ((x,y),direction,'M') in moves or ((x,y),direction,'J') in moves:
+				print "valid move"
 			# change the board somehow
 			self.board.generateSuccessorBoard((int(x),int(y)), direction)
 
