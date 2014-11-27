d0a1d4395efb60fb491790b1af0d9fcd175e01fe
Jonathan Taratuta-Titus <taratutatitus@college.harvard.edu>
2014-11-27 18:15:03 -0500

    finished refactoring Board stuff, should now generate new successor states

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ class Agent:
 class Move:
 	"""
 	unit tuples for adding to coordinates
+	unit scalars for jumps or regular moves
 	"""
 	NW = (-1,-1)
 	NE = (-1,1)
@@ -32,6 +33,28 @@ class Board:
 
 		self.pieces = [(0,5),(2,5),(4,5),(6,5),(1,6),(3,6),(5,6),(7,6),(0,7),(2,7),(4,7),(6,7)]
 		self.computer = [(1,0),(3,0),(5,0),(7,0),(0,1),(2,1),(4,1),(6,1),(1,2),(3,2),(5,2),(7,2)]
+	
+	def getValue(coordinate):
+		x,y = coordinate
+		return self.state[x][y]
+	
+	def setValue(coordinate, value):
+		x,y = coordinate
+		self.state[x][y] = value
+	
+	def removePiece(coordinate):
+		try:
+			self.pieces.remove(coordinate)
+		except ValueError:
+			try:
+				self.computer.remove(coordinate)
+			except ValueError:
+				pass
+	def addPiece(coordinate, who):
+		if who > 0:
+			self.pieces.append(coordinate)
+		else:
+			self.computer.append(coordinate)
 
 	def __str__(self):
 		""
@@ -57,20 +80,20 @@ class Board:
 			pieces_to_choose = self.human
 		else: # computer
 			pieces_to_choose = self.computer
-		for (x,y) in pieces_to_choose:
-			current_piece = self.state[x][y]
+		for coord in pieces_to_choose:
+			current_piece = self.getValue(coord)
 			# if the piece can move south, figure out possible SE and SW moves
 			if abs(current_piece) is 2 or current_piece < 0:
 				# for both SE and SW
 				for direction in [Move.SE, Move.SW]:
 					try:
-						nx,ny = Tuple.add((x,y),direction)
-						new_square = self.state[nx][ny]
-						jx,jy = Tuple.add((nx,ny),direction)
+						new_coord = Tuple.add(coord,direction)
+						new_square = self.getValue(new_coord)
+						jump_coord = Tuple.add(new_coord,direction)
 						if not new_square: # if empty square
-							valid_moves.append((x,y), direction, Move.M)
-						elif -1*who*new_square > 0 and not self.state[jx][jy]: # if can jump over
-							valid_jumps.append((x,y), direction, Move.J)
+							valid_moves.append(coord, direction, Move.M)
+						elif -1*who*new_square > 0 and not self.getValue(jump_coord): # if can jump over
+							valid_jumps.append(coord, direction, Move.J)
 					except IndexError: # ignore move if it would go out of bounds
 						pass
 			# if the piece can move north, figure out possible NE and NW moves
@@ -78,13 +101,13 @@ class Board:
 				# for both NE and NW
 				for direction in [Move.NE, Move.NW]:
 					try:
-						nx,ny = Tuple.add((x,y),direction)
-						new_square = self.state[nx][ny]
-						jx,jy = Tuple.add((nx,ny),direction)
+						new_coord = Tuple.add(coord,direction)
+						new_square = self.getValue(new_coord)
+						jump_coord = Tuple.add(new_coord,direction)
 						if not new_square: # if empty square
-							valid_moves.append((x,y),direction, Move.M)
-						elif -1*who*new_square > 0 and not self.state[jx][jy]: # if can jump over
-							valid_jumps.append((x,y), direction, Move.J)
+							valid_moves.append(coord,direction, Move.M)
+						elif -1*who*new_square > 0 and not self.getValue(jump_coord): # if can jump over
+							valid_jumps.append(coord, direction, Move.J)
 					except IndexError: # ignore move if it would go out of bounds
 						pass
 		# if a jump can be made, the person has to jump
@@ -94,20 +117,27 @@ class Board:
 			return valid_moves
 
 	def generateSuccessorBoard(self, pos, direction, magnitude):
-		x, y = pos
-		piece = self.state[x][y]
-		dist = 1
-		if jump:
-			dist = 2
-		self.state[x][y] = 0
-		if direction == 'NW':
-			self.state[x-dist][y-dist] = piece
-		if direction == 'NE':
-			self.state[x-dist][y+dist] = piece
-		if direction == 'SE':
-			self.state[x+dist][y+dist] = piece
-		if direction == 'SW':
-			self.state[x+dist][y-dist] = piece
+		"""
+		pos is current piece position
+		direction is tuple indicating unit to add to get new direction
+		magnitude is 1 or 2: 1 for no jump, 2 for jump
+		"""
+		# get piece value
+		piece = self.getValue(pos)
+
+		# set current position to be empty and remove from list
+		self.setValue(pos,0)
+		self.removePiece(pos)
+		# set position one diagonal away to be empty and remove from list
+		one_diagonal = Tuple.add(pos,direction)
+		self.setValue(one_diagonal,0)
+		self.removePiece(one_diagonal)
+		# set new position to have the value of the original piece, add back to list
+		new_pos = Tuple.add(pos,Tuple.c_multiple(magnitude,direction))
+		self.setValue(new_pos,piece)
+		self.addPiece(new_pos,piece)
+
+
 
 
 class Game:
