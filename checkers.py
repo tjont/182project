763c8c11f799715856c3c38cceed14ecb4ef072a
Matthew Timoney <mtimoney@college.harvard.edu>
2014-11-30 03:27:16 -0500

    can run simulations multiple times with command line

--- checkers.py
+++ checkers.py
@@ -5,9 +5,10 @@ from board import Board
 if __name__ == "__main__":
 	board = Board()
 	if len(sys.argv) is 3:
-		agent1 = sys.argv[1]
-		agent2 = sys.argv[2]
-		newGame = Game(board, agent1, agent2)
+		newGame = Game(board, sys.argv[1], sys.argv[2])
+		newGame.run()
+	elif len(sys.argv) is 4:
+		newGame = Game(board, sys.argv[1], sys.argv[2], sys.argv[3])
 		newGame.run()
 	else:
 		print "please format: python checkers.py agent1 agent2"
--- game.py
+++ game.py
@@ -2,13 +2,17 @@ from agents import RandomAgent
 from agents import MinimaxAgent
 from agents import AStarAgent
 from agents import HumanAgent
+from board import Board
 
 import sys
 import time
 
 class Game:
-	def __init__(self, board, agentType1, agentType2):
+	def __init__(self, board, agentType1, agentType2, simulations=1):
 		self.board = board
+
+		# number of times to simulate
+		self.simulations = int(simulations)
 
 		agent_dict = {"RandomAgent":RandomAgent,"MinimaxAgent":MinimaxAgent,"AStarAgent":AStarAgent,"HumanAgent":HumanAgent}
 		
@@ -25,22 +29,45 @@ class Game:
 		# list of how long it took an agent to make a move for each move
 		self.agent1_times = []
 		self.agent2_times = []
-
+	
+		# totals
+		self.agent1_wins = 0 
+		self.agent2_wins = 0
 
 	def exit(self, who):
 		# "who" is the agent who lost
 		if who == 1:
-			print "Agent 2 wins!"
-			print "Number of moves: " + str(len(self.agent2_moves))
-			print "Average time per move: " + str(sum(self.agent2_times) / len(self.agent2_times))[:5] + " seconds"
+			self.agent2_wins += 1
+			print "Agent 2 wins with " + str(len(self.agent2_moves)) + " moves\n"
+			#print "Average time per move: " + str(sum(self.agent2_times) / len(self.agent2_times))[:5] + " seconds"
 		else:
-			print "Agent 1 wins!"
-			print "Number of moves: " + str(len(self.agent1_moves))
-			print "Average time per move: " + str(sum(self.agent1_times) / len(self.agent1_times))[:5] + " seconds"
-		sys.exit(0)
+			self.agent1_wins += 1
+			print "Agent 1 wins with " + str(len(self.agent1_moves)) + " moves\n"
+			#print "Average time per move: " + str(sum(self.agent1_times) / len(self.agent1_times))[:5] + " seconds"
+		
+		if self.simulations != 0:
+			# reset all the single game statistics
+			self.agent1_moves = []
+			self.agent2_moves = []
+			self.agent1_times = []
+			self.agent2_times = []
+
+			# reset the board
+			self.board = Board()
+
+			self.run()
+
+		else:
+			print "================ TOTALS ================"
+			print "Agent 1 won " + str(self.agent1_wins) + " times"
+			print "Agent 2 won " + str(self.agent2_wins) + " times"
+			sys.exit(0)
 
 
 	def run(self):
+		# log this game
+		self.simulations -= 1
+		
 		while True:
 			print "Agent 1's (O) turn "
 			print self.board			
